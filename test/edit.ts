4fc9b9df3cd3a6181df81207795101a371341086
Twisuki <Twisuki@outlook.com>
2026-03-26 15:37:00 +0800

    :sparkles: 测试与添加测试文档

---
+++ test/edit.ts
@@ -0,0 +1,34 @@
+import "dotenv/config"
+import { Taskify, TaskListQuery, TaskQuery } from "../src/index.js"
+
+async function main() {
+	const taskify = new Taskify({
+		refreshToken: process.env.GOOGLE_TASK_REFRESH_TOKEN!,
+		clientId: process.env.GOOGLE_TASK_CLIENT_ID!,
+		clientSecret: process.env.GOOGLE_TASK_CLIENT_SECRET!,
+	})
+
+	await taskify.login()
+
+	const lists = await taskify.loadTaskLists()
+	const list = new TaskListQuery(lists).titleLike("测试").first()
+
+	if (list) {
+		const tasks = await taskify.loadTasks(list.id)
+		const task = new TaskQuery(tasks).titleLike("测试").first()
+
+		if (task) {
+			taskify.editTask(list.id, task.id, t => ({
+				...t,
+				title: "修改后的测试任务",
+			}))
+		}
+	}
+
+	await taskify.commit()
+}
+
+main().catch(err => {
+	console.error(err)
+	process.exit(1)
+})
